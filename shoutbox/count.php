f83e0d64007ac1697662e478cb3af9570d48bc68
ꦧꦭ꧀ <iqbalrifai@waifu.club>
2018-11-07 18:20:42 +0700

    Revert "Add Shoutbox and Blog Post"

--- shoutbox/count.php
+++
@@ -1,8 +0,0 @@
-<?php
-$all = mysql_result(mysql_query('SELECT COUNT(*) FROM `shout`'), 0);
-$new = mysql_result(mysql_query('SELECT COUNT(*) FROM `shout` WHERE `time` > "'.(time()-86400).'"'), 0);
-if($new == 0)
-$new = NULL;
-else $new = '/+<font class="off">'.$new.'</font>';
-echo $all.$new;
-?>
